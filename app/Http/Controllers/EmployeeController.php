562b0c6d06a4da8d0fd0081a60d27d8035293027
Ralfhstr <69681774+Ralfhstr@users.noreply.github.com>
2023-05-24 14:35:00 +0700

    Update EmployeeController.php

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -137,16 +137,13 @@ class EmployeeController extends Controller
         DB::table('employees')
         ->where('id', $id)
         ->update([
-            'firstname' => $request->input('firstName'),
-            'lastname' => $request->input('lastName'),
-            'email' => $request->input('email'),
-            'age' => $request->input('age'),
-            'position_id' => $request->input('position')
+            'firstname' => $request->firstName,
+            'lastname' => $request->lastName,
+            'email' => $request->email,
+            'age' => $request->age,
+            'position_id' => $request->position,
         ]);
         return redirect()->route('employees.index');
-
-
-    return view('employee.index', compact('pageTitle', 'employee'));
     }
 
     /**
